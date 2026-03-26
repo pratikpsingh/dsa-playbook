22d95a35ede425d2573316902124e13c3b7bbd8d
Pratik Singh <152969436+pratikpsingh@users.noreply.github.com>
2026-03-27 01:36:20 +0530

    Time: 315 ms (22.09%) | Memory: 19.3 MB (85.38%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.py
+++ 1656-count-good-triplets/count-good-triplets.py
@@ -5,7 +5,7 @@ class Solution:
         length = len(arr)
 
         for i in range(length - 2):
-            for j in range(i+1, length - 1):
+            for j in range(i + 1, length - 1):
                 for k in range(j + 1, length):
 
                     if (abs(arr[i] - arr[j]) <= a) and (abs(arr[j] - arr[k]) <= b) and (abs(arr[i] - arr[k]) <= c):
