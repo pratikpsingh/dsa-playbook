04415eb6454e699fa44b3f956d7e511ab7a12cf3
Pratik Singh <152969436+pratikpsingh@users.noreply.github.com>
2026-03-30 23:58:52 +0530

    Time: 3 ms (60.53%) | Memory: 20.3 MB (86.87%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        length = len(nums)
-        total_sum = length * (length + 1) // 2
+        # length = len(nums)
+        # total_sum = length * (length + 1) // 2
 
-        return total_sum - sum(nums)
+        # return total_sum - sum(nums)
         
+        xor_all = 0
+        for i in range(len(nums) + 1):
+            xor_all ^= i
+
+        for num in nums:
+            xor_all ^= num
+
+        return xor_all
+
+        
+
