71550b0dfa6e5d57462e9491e75bad6cfd50e978
Pratik Singh <152969436+pratikpsingh@users.noreply.github.com>
2026-03-31 21:43:41 +0530

    Time: 15 ms (46.46%) | Memory: 20.4 MB (49.46%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.py
+++ 875-longest-mountain-in-array/longest-mountain-in-array.py
@@ -1,47 +1,101 @@
 class Solution:
-    def longestMountain(self, arr: List[int]) -> int:
-        start = -1
-        peak = -1
-        max_length = 0
 
-        for i in range(1, len(arr)):
+    # Method 01
+    # def longestMountain(self, arr: List[int]) -> int:
+    #     start = -1
+    #     peak = -1
+    #     max_length = 0
 
-            if arr[i] > arr[i - 1]:
-                if peak != -1:
-                    max_length = max(max_length, (i - 1 - start + 1))
-                    peak = -1
-                    start = i - 1
-                    continue
+    #     for i in range(1, len(arr)):
 
-                if start == -1:
-                    start = i - 1
-                continue
+    #         if arr[i] > arr[i - 1]:
+    #             if peak != -1:
+    #                 max_length = max(max_length, (i - 1 - start + 1))
+    #                 peak = -1
+    #                 start = i - 1
+    #                 continue
+
+    #             if start == -1:
+    #                 start = i - 1
+    #             continue
                     
             
-            if arr[i] == arr[i - 1]:
-                if peak != -1:
-                    if peak != i - 1:
-                        max_length = max(max_length, (i - 1) - start + 1)
-                        peak = -1
-                        start = -1
-                else:
-                    start = -1
+    #         if arr[i] == arr[i - 1]:
+    #             if peak != -1:
+    #                 if peak != i - 1:
+    #                     max_length = max(max_length, (i - 1) - start + 1)
+    #                     peak = -1
+    #                     start = -1
+    #             else:
+    #                 start = -1
 
-                continue
+    #             continue
 
-            if arr[i] < arr[i - 1]:
-                if start == -1:
-                    continue
+    #         if arr[i] < arr[i - 1]:
+    #             if start == -1:
+    #                 continue
 
-                if peak == -1:
-                    peak = i - 1
+    #             if peak == -1:
+    #                 peak = i - 1
 
-                continue
+    #             continue
 
-        # if max_length == 0 and peak != -1:
-        #     max_length = i - start + 1
+    #     # if max_length == 0 and peak != -1:
+    #     #     max_length = i - start + 1
 
-        if peak != -1:
-            max_length = max(max_length, i - start + 1)
+    #     if peak != -1:
+    #         max_length = max(max_length, i - start + 1)
 
-        return max_length
+    #     return max_length
+
+        # Method 02
+        def longestMountain(self, arr: List[int]) -> int:
+            # Find all picks and find the length
+            longest_mountain = 0
+
+            for i in range(1, len(arr) - 1):
+                count = 0
+                if arr[i - 1] < arr[i] and arr[i] > arr[i + 1]:
+                    j = i
+                    count = 1 # for peak -> count peak
+                    while j > 0 and arr[j] > arr[j - 1]:
+                        count += 1
+                        j -= 1
+
+                    j = i
+                    while j < len(arr) - 1 and arr[j] > arr[j + 1]:
+                        count += 1
+                        j += 1
+
+                    longest_mountain = max(longest_mountain, count)
+
+            return longest_mountain
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
