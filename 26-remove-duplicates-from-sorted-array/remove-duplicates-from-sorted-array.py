fdbcb4315b58996e046d10c02e5eed45231f8cd0
Pratik Singh <152969436+pratikpsingh@users.noreply.github.com>
2026-04-05 23:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (27.45%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,36 +1,49 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
 
-        unique_element = None
-        swap_index = -1
-        count = 0
+        # unique_element = None
+        # swap_index = -1
+        # count = 0
 
-        for index, num in enumerate(nums):
-            if index == 0:
-                unique_element = num
-                count += 1
-                continue
+        # for index, num in enumerate(nums):
+        #     if index == 0:
+        #         unique_element = num
+        #         count += 1
+        #         continue
 
-            if num ==  unique_element and swap_index == -1:
-                swap_index = index
-                continue
-            elif num == unique_element and swap_index != -1:
-                continue
-
-
-            if num != unique_element and swap_index != -1:
-                count += 1
-                nums[swap_index] = num
-                swap_index += 1
-                unique_element = num
-                continue
-            elif num != unique_element and swap_index == -1:
-                unique_element = num
-                count += 1
-                continue
+        #     if num ==  unique_element and swap_index == -1:
+        #         swap_index = index
+        #         continue
+        #     elif num == unique_element and swap_index != -1:
+        #         continue
 
 
-        return count
+        #     if num != unique_element and swap_index != -1:
+        #         count += 1
+        #         nums[swap_index] = num
+        #         swap_index += 1
+        #         unique_element = num
+        #         continue
+        #     elif num != unique_element and swap_index == -1:
+        #         unique_element = num
+        #         count += 1
+        #         continue
+
+
+        # return count
+
+
+
+        # METHOD 2
+        swap_index=1
+
+        for index in range(1,len(nums)):
+
+            if nums[index]!=nums[swap_index-1]:
+                nums[swap_index]=nums[index]
+                swap_index+=1
+                
+        return swap_index
                     
             
 
