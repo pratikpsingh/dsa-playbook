4a672991af0463eeaf8159277219827f3c755c43
Pratik Singh <152969436+pratikpsingh@users.noreply.github.com>
2026-04-08 00:45:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (66.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # if n ==1:
-        #     return 1
-
-        # if n == 0:
+        # if n <= 1:
         #     return 1
 
         # return self.climbStairs(n - 1) + self.climbStairs(n - 2)
 
+        # Using DP:
         count = [1, 1]
         for i in range(2, n + 1):
             count.append(count[i - 1] + count[i - 2])
