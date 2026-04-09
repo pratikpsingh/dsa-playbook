9c1101c7d33a43ea8f022908ef2bf821c896fd81
Pratik Singh <152969436+pratikpsingh@users.noreply.github.com>
2026-04-09 23:04:00 +0530

    Time: 8 ms (49.78%) | Memory: 19.7 MB (64.33%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        if s.strip() == '' or len(s) == 1:
+            return True
+
+        i = 0 
+        j = len(s) - 1
+        valid_chars = set('abcdefghijklmnopqrstuvwxyz1234567890')
+        while i <= j:
+            while s[i] not in valid_chars:
+                i += 1
+                if i == len(s):
+                    return True
+            while s[j] not in valid_chars:
+                j -= 1
+                if j == i:
+                    return True
+
+            if i == j:
+                return True
+
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+
+        return True
+            
+
+
+        
+        
