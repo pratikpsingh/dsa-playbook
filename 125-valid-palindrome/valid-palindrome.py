2d018f9aa39b1d2dc53527fbe32c6bbd5bbdfae3
Pratik Singh <152969436+pratikpsingh@users.noreply.github.com>
2026-04-09 23:11:08 +0530

    Time: 9 ms (42.28%) | Memory: 19.6 MB (83.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,34 +1,48 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        if s.strip() == '' or len(s) == 1:
-            return True
+        # s = s.lower()
+        # if s.strip() == '' or len(s) == 1:
+        #     return True
 
-        i = 0 
+        # i = 0 
+        # j = len(s) - 1
+        # valid_chars = set('abcdefghijklmnopqrstuvwxyz1234567890')
+        # while i <= j:
+        #     while s[i] not in valid_chars:
+        #         i += 1
+        #         if i == len(s):
+        #             return True
+        #     while s[j] not in valid_chars:
+        #         j -= 1
+        #         if j == i:
+        #             return True
+
+        #     if i == j:
+        #         return True
+
+        #     if s[i] == s[j]:
+        #         i += 1
+        #         j -= 1
+        #     else:
+        #         return False
+
+        # return True
+
+
+        i = 0
         j = len(s) - 1
-        valid_chars = set('abcdefghijklmnopqrstuvwxyz1234567890')
-        while i <= j:
-            while s[i] not in valid_chars:
-                i += 1
-                if i == len(s):
-                    return True
-            while s[j] not in valid_chars:
-                j -= 1
-                if j == i:
-                    return True
 
-            if i == j:
-                return True
-
-            if s[i] == s[j]:
+        while i < j:
+            while i < j and not s[i].isalnum():
                 i += 1
+
+            while i < j and not s[j].isalnum():
                 j -= 1
-            else:
+
+            if s[i].lower() != s[j].lower():
                 return False
 
+            i += 1
+            j -= 1
+
         return True
-            
-
-
-        
-        
